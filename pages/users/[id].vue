17eee6e1d79da29882c30d85c177c3b2fa2036dc
Bruno <moyse94@gmail.com>
2023-04-08 23:14:47 +0200

    Update detail user

--- pages/users/[id].vue
+++ pages/users/[id].vue
@@ -1,21 +1,100 @@
 <template>
     <div>
-        <h2>
-            {{ player.firstName }} {{ player.lastName }}
+        <!-- Player's name-->
+        <h2 class="text-center text-3xl font-bold">
+            {{ player.firstName }} {{ player.lastName.toUpperCase() }}
         </h2>
+
+        <!-- Main win/results-->
+        <div class="my-8">
+            <div class="grid grid-cols-2">
+                <div class="rounded-xl border border-gray-500 p-6 mr-3">
+                    <div class="text-center text-2xl mb-1">
+                        {{ getTotalEarnings(player.tournamentResults) }}
+                    </div>
+                    <div class="text-center text-sm">
+                        Total gagné
+                    </div>
+                </div>
+                <div class="rounded-xl border border-gray-500 p-6 ml-3">
+                    <div class="text-center text-2xl mb-1">
+                        {{ getBestCash(player.tournamentResults) }}
+                    </div>
+                    <div class="text-center text-sm">
+                        Meilleur résulat
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- History -->
+        <div class="flex flex-col mt-6 -mx-4">
+            <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                    <div class="shadow overflow-hidden sm:rounded-lg">
+                        <table class="min-w-full text-sm">
+                            <thead class="bg-gray-600 text-xs uppercase font-medium text-white">
+                            <tr>
+                                <th scope="col" class="px-3 py-3 text-left tracking-wider">
+                                    Date
+                                </th>
+                                <th scope="col" class="px-3 py-3 text-left tracking-wider">
+                                    Tournoi
+                                </th>
+                                <th scope="col" class="px-3 py-3 text-left tracking-wider">
+                                    Pos.
+                                </th>
+                                <th scope="col" class="px-3 py-3 text-left tracking-wider">
+                                    Rés.
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody class="bg-gray-100">
+                            <tr
+                                v-for="(result, index) in orderResults(player.tournamentResults)"
+                                :key="result.id"
+                                :class="index % 2 === 1 ? 'bg-black bg-opacity-10' : ''"
+                            >
+                                <td class="px-3 py-4 whitespace-nowrap">
+                                    <div>{{ getDateFormattedDay(result?.tournament?.scheduledAt) }}
+                                        {{ getDateFormattedMonth(result?.tournament?.scheduledAt) }}</div>
+                                    <div>{{ getDateFormattedYear(result?.tournament?.scheduledAt) }}</div>
+                                </td>
+                                <td class="px-3 py-4 whitespace-nowrap">
+                                    {{ result?.tournament?.name }}
+                                </td>
+                                <td class="px-3 py-4 whitespace-nowrap">
+                                    {{
+                                        result?.position && result.position === 1
+                                            ? result.position + 'er'
+                                            : result.position + 'e'
+                                    }}
+                                </td>
+                                <td class="px-3 py-4 whitespace-nowrap">
+                                    {{ result?.prize + '€' }}
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
 
 import {Ref} from "vue";
-import {Tournament, User} from "@prisma/client";
+import {Tournament, User, TournamentResult} from "@prisma/client";
+
+const config = useRuntimeConfig();
 
 const {params} = useRoute();
 const userId: string = params.id as string
 
 const getUser = async (userId: string) => {
-    const {data, pending, refresh, error} = await useFetch('http://localhost:3000/api/graphql', {
+    const {data, pending, refresh, error} = await useFetch(config.public.apiBase, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -45,6 +124,13 @@ const getUser = async (userId: string) => {
                 }
                 tournamentResults {
                     id
+                    prize
+                    position
+                    tournament {
+                        id
+                        name
+                        scheduledAt
+                    }
                 }
             }
         }`,
@@ -59,6 +145,49 @@ const getUser = async (userId: string) => {
     if (data) return data as Ref<User>;
     else throw createError({statusCode: 404, message: 'L\'utilisateur n\'a pas été trouvé'});
 
+}
+
+const getTotalEarnings = (results: TournamentResult[]) => {
+    // make the sum of all the result.prize
+    let totalEarnings = 0;
+    results.forEach(result => {
+        if (result?.prize !== null) totalEarnings += result?.prize;
+    });
+    return Math.floor(totalEarnings).toLocaleString('fr-BE', {style:"currency", currency:"EUR", minimumFractionDigits: 0});
+};
+
+const getBestCash = (results: TournamentResult[]) => {
+    // make the sum of all the result.prize
+    let bestCash = 0;
+    results.forEach(result => {
+        if (result?.prize !== null && result.prize > bestCash) bestCash = result?.prize;
+    });
+    return Math.floor(bestCash).toLocaleString('fr-BE', {style:"currency", currency:"EUR", minimumFractionDigits: 0});
+};
+
+const orderResults = (results: TournamentResult[]) => {
+    return results.sort((a, b) => {
+        return new Date(b.tournament.scheduledAt).getTime() - new Date(a.tournament.scheduledAt).getTime();
+    });
+}
+
+const getDateFormattedYear = (date: Date) => {
+    const dateObj = new Date(date);
+    return dateObj.toLocaleString('fr-FR', {
+        year: 'numeric'
+    });
+}
+const getDateFormattedMonth = (date: Date) => {
+    const dateObj = new Date(date);
+    return dateObj.toLocaleString('fr-FR', {
+        month: "short"
+    });
+}
+const getDateFormattedDay = (date: Date) => {
+    const dateObj = new Date(date);
+    return dateObj.toLocaleString('fr-FR', {
+        day: 'numeric'
+    });
 }
 
 // fetching data

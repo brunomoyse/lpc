4b56851c8fde7cc2f1329e4e3737e5decc018d64
Bruno <moyse94@gmail.com>
2023-04-14 15:45:45 +0200

    Add server middleware

---
+++ server/middleware/auth.ts
@@ -0,0 +1,60 @@
+import { H3Event } from "h3";
+import { JwtPayload, verify } from "jsonwebtoken";
+import { parse } from "graphql";
+import { readBody } from "h3";
+import { getCookie } from "h3";
+import {useRouter} from "vue-router";
+const config = useRuntimeConfig();
+
+
+
+const checkIfLoginMutation = async (event: H3Event):Promise<Boolean> => {
+    const body = await readBody(event);
+    const query: string = body.query;
+    const document = parse(query);
+
+    const operation = document.definitions.find(
+        (definition) => definition.kind === "OperationDefinition"
+    );
+    let operationType = null;
+    let operationName = null;
+    if (operation && "operation" in operation) {
+        operationType = operation && operation.operation;
+    }
+    if (operation && "name" in operation) {
+        operationName = operation && operation.name && operation.name.value;
+    }
+
+    // Check if the operation is the login mutation
+    return operationType === "mutation" && operationName === "LoginUser";
+}
+
+const verifyToken = (event: H3Event) => {
+    const access_token = getCookie(event, "access-token");
+    if (access_token) {
+        const jwtPayload: string | JwtPayload = verify(
+            access_token,
+            config.private.appSecret
+        );
+        if (typeof jwtPayload === 'object') {
+            const currentUserId = jwtPayload.userId;
+            event.context.auth = {currentUserId};
+        }
+    } else {
+        throw new Error("No token provided");
+    }
+}
+
+
+export default defineEventHandler(async (event: H3Event) => {
+    if (event.node.req.url !== "/" ) {
+        if (event.node.req.url === "/api/graphql") {
+            const isLoginMutation = await checkIfLoginMutation(event);
+            if (!isLoginMutation) {
+                verifyToken(event);
+            }
+        } else {
+            verifyToken(event);
+        }
+    }
+});

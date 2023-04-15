3f5ee31ebd7a3a80af47b23b5b6ccb2b751eaf9a
Bruno <moyse94@gmail.com>
2023-04-15 21:37:35 +0200

    Add user store

---
+++ stores/userStore.ts
@@ -0,0 +1,20 @@
+import {defineStore} from "pinia";
+import {useApiService} from "@/apiService";
+import {User} from "@prisma/client";
+
+export const useUserStore = defineStore({
+    id: "userStore",
+    state: () => ({
+        currentUser: null as User|null,
+    }),
+    actions: {
+        async loginUser(args: any) {
+            const currentUser = await useApiService.loginUser(args);
+            if (currentUser) this.currentUser = currentUser.value;
+        },
+        async logoutUser() {
+            await useFetch('/logout');
+            this.currentUser = null;
+        }
+    }
+})

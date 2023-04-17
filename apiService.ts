8ba86a64c3dd07dc80933594cb7244c95a2226c7
Bruno <moyse94@gmail.com>
2023-04-17 21:49:43 +0200

    Update apiService.js use localStorage token

--- apiService.ts
+++ apiService.ts
@@ -1,5 +1,6 @@
 import {Ref} from "vue";
 import {Tournament, User} from "@prisma/client";
+
 // @todo - useRuntimeConfig() is not working here
 //const config = useRuntimeConfig();
 let config = {
@@ -7,6 +8,10 @@ let config = {
         apiBase: "http://localhost:3000/api/graphql"
     }
 }
+
+//@ts-ignore
+const authToken = localStorage.getItem('access-token');
+
 export const useApiService = {
     getTournamentDetail: async (args: any): Promise<Ref<Tournament>> => {
         const tournamentId: string = args.tournamentId;
@@ -15,6 +20,7 @@ export const useApiService = {
             headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json',
+                'Authorization': 'Bearer ' + authToken
             },
             body: JSON.stringify({
                 query: `query (
@@ -72,6 +78,76 @@ export const useApiService = {
         if (data) return data as Ref<Tournament>;
         else throw createError({statusCode: 404, message: 'Le tournoi n\'a pas été trouvé'});
     },
+    getTournaments: async (args: any): Promise<Ref<{ futureTournaments: [Tournament], pastTournaments: [Tournament] }>> => {
+        const { paginationSettings } = args;
+        let { data, pending, refresh, error } = await useFetch(config.public.apiBase, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': 'Bearer ' + authToken
+            },
+            body: JSON.stringify({
+                query: `query (
+                $take: Int
+                $skip: Int
+                $userId: ID
+                $startingRange: Date
+                $endingRange: Date
+            ) {
+                futureTournaments: tournaments (
+                    take: $take
+                    skip: $skip
+                    userId: $userId
+                    startingRange: $startingRange
+                ){
+                    id
+                    name
+                    scheduledAt
+                    lateRegistrationAt
+                    imgPath
+                    tournamentTags {
+                        name
+                    }
+                }
+                pastTournaments: tournaments (
+                    take: $take
+                    skip: $skip
+                    userId: $userId
+                    endingRange: $endingRange
+                ){
+                    id
+                    name
+                    scheduledAt
+                    lateRegistrationAt
+                    imgPath
+                    tournamentTags {
+                        name
+                    }
+                }
+            }`,
+                variables: {
+                    take: paginationSettings.perPage,
+                    skip: paginationSettings.skippedCount,
+                    startingRange: new Date().toISOString(),
+                    endingRange: new Date().toISOString()
+                }
+            }),
+            transform: (res: any) => res.data,
+        });
+        if (error?.value) {
+            if (error.value?.data?.message === 'Token expired') {
+                await useFetch('/logout');
+                //@ts-ignore
+                localStorage.removeItem('access-token');
+                navigateTo('/login');
+            }
+            console.error(error.value);
+            throw createError({ statusCode: 404, message: 'Les tournois n\'ont pas été trouvés' });
+        }
+        if (data) return data;
+        else throw createError({ statusCode: 404, message: 'Les tournois n\'ont pas été trouvés' });
+    },
     registerTournament: async (args: any): Promise<Ref<Tournament>> => {
         const {tournamentId, userId} = args;
         const {data, pending, refresh, error} = await useFetch(config.public.apiBase, {
@@ -79,6 +155,7 @@ export const useApiService = {
             headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json',
+                'Authorization': 'Bearer ' + authToken
             },
             body: JSON.stringify({
                 query: `mutation (
@@ -145,6 +222,7 @@ export const useApiService = {
             headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json',
+                'Authorization': 'Bearer ' + authToken
             },
             body: JSON.stringify({
                 query: `mutation (
@@ -204,13 +282,13 @@ export const useApiService = {
         if (data) return data as Ref<Tournament>;
         else throw createError({statusCode: 500, message: 'La désinscription n\'a pas abouti'});
     },
-    loginUser: async (args: any): Promise<Ref<User>> => {
+    loginUser: async (args: any): Promise<any> => {
         const {email, password} = args;
         const {data, pending, refresh, error} = await useFetch(config.public.apiBase, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                'Accept': 'application/json',
+                'Accept': 'application/json'
             },
             body: JSON.stringify({
                 query: `mutation LoginUser(
@@ -221,9 +299,12 @@ export const useApiService = {
                 email: $email
                 password: $password
             ){
-                id
-                lastName
-                firstName
+                user {
+                    id
+                    lastName
+                    firstName
+                }
+                token
             }
         }`,
                 variables: {
@@ -235,8 +316,9 @@ export const useApiService = {
         });
 
         if (error?.value) console.error(error.value);
-        if (data) return data as Ref<User>;
+        if (data) return data;
         else throw createError({statusCode: 500, message: 'La connection a échoué'});
-    }
+    },
+
 }
 
